07fc16d6c4abf66a738a8ebd5719950074a02b25
rixong <rixong@gmail.com>
2020-08-22 11:29:35 -0500

    setup controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,10 @@
 class ListsController < ApplicationController
+
+  def index
+    list_uid = params[:list_id]
+    list_id = List.find_by(uid: list_uid).id
+    lists = Name.where("uid = ?", list_uid)
+    render json: lists
+  end
+
 end
--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,2 +1,3 @@
 class NamesController < ApplicationController
+
 end
