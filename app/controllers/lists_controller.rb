a474224638527246c3fa86590d591270887aad75
rixong <rixong@gmail.com>
2020-08-22 11:49:56 -0500

    Added logic to handle list ID submission

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,9 +2,16 @@ class ListsController < ApplicationController
 
   def index
     list_uid = params[:list_id]
-    list_id = List.find_by(uid: list_uid).id
-    lists = Name.where("uid = ?", list_uid)
-    render json: lists
+    if list_uid.empty?
+      render json: {error: "Creat new ID"}
+    end
+    list_id = List.find_by(uid: list_uid)
+    if !list_id
+      render json: {error: "No such ID"}
+    else
+      lists = Name.where("uid = ?", list_uid)
+      render json: {id:list_id, names: lists}
+    end
   end
 
 end
